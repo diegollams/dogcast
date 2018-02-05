066e1a0145d155acdc322bddff142b91c9adf8e9
Diego Llamas <diegollams@gmail.com>
2018-02-04 19:12:52 -0600

    Install jquery

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :dogcast, Dogcast.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "dogcast_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :dogcast, Dogcast.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "dogcast_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
