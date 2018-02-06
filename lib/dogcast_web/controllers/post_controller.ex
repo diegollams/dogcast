2fc5b927b87ff44ddc30761e4ecec2b695ab7728
Diego Llamas <diegollams@gmail.com>
2018-02-06 08:21:31 -0600

    Add commit form

--- lib/dogcast_web/controllers/post_controller.ex
+++ lib/dogcast_web/controllers/post_controller.ex
@@ -2,7 +2,11 @@ defmodule DogcastWeb.PostController do
   use DogcastWeb, :controller
 
   alias Dogcast.DogcastWeb
-  alias Dogcast.DogcastWeb.Post
+  alias Dogcast.Post
+  alias Dogcast.Comment
+  alias Dogcast.Repo
+
+  plug :scrub_params, "comment" when action in [:add_comment]
 
   def index(conn, _params) do
     posts = DogcastWeb.list_posts()
@@ -26,8 +30,9 @@ defmodule DogcastWeb.PostController do
   end
 
   def show(conn, %{"id" => id}) do
-    post = DogcastWeb.get_post!(id)
-    render(conn, "show.html", post: post)
+    post = Repo.get(Post, id) |> Repo.preload([:comments])
+    changeset = Comment.changeset(%Comment{}, %{"content" => "Titulo"})
+    render(conn, "show.html", post: post, changeset: changeset)
   end
 
   def edit(conn, %{"id" => id}) do
@@ -46,6 +51,19 @@ defmodule DogcastWeb.PostController do
         |> redirect(to: post_path(conn, :show, post))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", post: post, changeset: changeset)
+    end
+  end
+
+  def add_comment(conn,  %{"comment" => comment_params, "post_id" => post_id}) do
+    changeset = Comment.changeset(%Comment{}, Map.put(comment_params, "post_id", post_id))
+    post      = Post |> Repo.get(post_id) |> Repo.preload([:comments])
+    if changeset.valid? do
+      Repo.insert(changeset)
+      conn
+      |> put_flash(:info, "Comment added.")
+      |> redirect(to: post_path(conn, :show, post))
+    else
+      render(conn, "show.html", post: post, changeset: changeset)
     end
   end
 
